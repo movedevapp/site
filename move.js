b0754bc54c736a0aacdee2d54bf1b04fb79c64d8
movedevapp <moveape@gmail.com>
2025-01-25 15:55:24 -0300

    Add files via upload

---
+++ move.js
@@ -0,0 +1,9 @@
+<script>
+    // Toggle do menu hambúrguer
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+</script>
