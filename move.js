a01ecd624394a1ceb414e48e8c4dbf18130c8759
Pedro Silva <103220595+Dboyp@users.noreply.github.com>
2025-04-07 15:15:52 -0300

    Delete move.js

--- move.js
+++
@@ -1,9 +0,0 @@
-<script>
-    // Toggle do menu hambúrguer
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-</script>
